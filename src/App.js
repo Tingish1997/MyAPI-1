57751bd55425604a64b44d1a2ef09fca5806b9d2
Tingish1997 <82696247+Tingish1997@users.noreply.github.com>
2021-04-29 16:29:19 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,6 @@ import { theme } from "./theme";
 import PrimarySearchAppBar from "./PrimarySearchAppBar";
 import {ThemeProvider} from "@material-ui/core";
 
-class App{
-  api(){
-    return fetch("http://www.mocky.io/v2/5e3d41272d00003f7ed95c09")
-    .then((response)=>{
-      return response.json
-    })
-  }   
-}
-
 function App() {
   return (
     <>
